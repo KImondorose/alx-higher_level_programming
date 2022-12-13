7012d6ca980c4ebb3b50a9c877eaaae1e189c3e4
KImondorose <rosewairimu1@gmail.com>
2022-12-13 06:53:39 +0300

    Correct tebalpha

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,5 @@
-!/usr/bin/python3
-for i in 'zYxWvUtSrQpOnMlKjIhGfEdCbA':
-    print("{}".format(i), end="")
+
+
+#!/usr/bin/python3
+for ch in reversed(range(97, 123)):
+    print("{:c}".format(ch if (ch % 2 == 0) else (ch - 32)), end='')
